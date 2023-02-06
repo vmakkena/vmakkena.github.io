d27ac33596de5fbf1e22cddc1414421e2880f5d5
Venkat Teja Makkena <venkattejamakkena@Venkats-MacBook-Air.local>
2023-02-06 15:33:44 -0600

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Post1 from './posts/post1';
+
+function App() {
+  return (
+    <div>
+      <h1>My travel blog</h1>
+      <Post1 />
+    </div>
+  );
+}
