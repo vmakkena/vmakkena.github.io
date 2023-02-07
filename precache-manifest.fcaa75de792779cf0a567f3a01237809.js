71375aa2acd9ea5ed978726be3527a5f518480c4
Venkat Teja Makkena <venkattejamakkena@Venkats-MacBook-Air.local>
2023-02-06 21:49:32 -0600

    update build files

---
+++ precache-manifest.fcaa75de792779cf0a567f3a01237809.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ffed3f6f61f35fde994",
+    "url": "/static/css/main.7e5e0287.chunk.css"
+  },
+  {
+    "revision": "0ffed3f6f61f35fde994",
+    "url": "/static/js/main.0ffed3f6.chunk.js"
+  },
+  {
+    "revision": "4587b35573b7dda0a77d",
+    "url": "/static/js/1.4587b355.chunk.js"
+  },
+  {
+    "revision": "ff813772de7efb309245",
+    "url": "/static/js/2.ff813772.chunk.js"
+  },
+  {
+    "revision": "1f46550c42e737540ce6",
+    "url": "/static/js/runtime~main.1f46550c.js"
+  },
+  {
+    "revision": "dc9e125913aab7f198ad4375d963e5b8",
+    "url": "/index.html"
+  }
+];
